294deba841c81e7475fcae2a6526558300a0d99e
Stephen Chu <stchu777@gmail.com>
2017-08-13 12:58:42 -0700

    refactor: Redid C soln to p0003
    Realized I could simply use my python solution. It was just as fast and did
    not require additional functions.

--- c_projecteuler_solutions/p0003.c
+++ c_projecteuler_solutions/p0003.c
@@ -5,75 +5,25 @@ The prime factors of 13195 are 5, 7, 13 and 29.
 
 What is the largest prime factor of the number 600851475143?
 */
-
-/*
-The following C solution is from "Fuxino" who posted to projecteuler.net
-on 15 July 2017.
-
-The solution is not mine. However, I found it to be efficient and elegant
-beyond what I know at the moment, so I am using this as a study guide for
-myself.
-*/
 #include <stdio.h>
-#include <math.h>
 
-int is_prime(int num);
-long long max_prime_factor(long long num);
-
-int main(int argc, char **argv)
+int main(void)
 {
-   long long res, num=600851475143;
+    long long prime = 600851475143,
+	          a = 0,
+	          x = 1;
+	          
+	while (x <= prime) {
+		if (prime % x == 0) {
+			a = x;
+			prime = prime / x;
+		}
+		x++;
+	}
+	
+	printf("%lld\n", a);
 
-   res=max_prime_factor(num);
-
-   printf("%lld\n", res);
-
-   return 0;
-}
-
-long long max_prime_factor(long long num)
-{
-   int i, limit;
-
-   if(is_prime(num))
-      return num;
-
-   if(num%2==0)
-      return max_prime_factor(num/2);
-   else
-   {
-      limit=floor(sqrt(num));
-
-      for(i=3; i<=limit; i+=2)
-      {
-         if(num%i==0)
-            if(is_prime(i))
-               return max_prime_factor(num/i);
-      }
-   }
-
-   return -1;
-}
-
-int is_prime(int num)
-{
-   int i, limit;
-
-   if(num<=3)
-      return num==2 || num==3;
-
-   if(num%2==0 || num%3==0)
-      return 0;
-
-   limit=floor(sqrt(num));
-
-   for(i=5; i<=limit; i+=6)
-   {
-      if(num%i==0 || num%(i+2)==0)
-         return 0;
-   }
-
-   return 1;
+	return 0;
 }
 
 /*
